8a4a79208045e91b41fce847dce98ea4b7f7b3bc
Biprodas <biprodas.cse@gmail.com>
2019-03-04 00:27:17 +0600

    server side support for clean urls

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "049d802ff36a5ef7991b";
+/******/ 	var hotCurrentHash = "e4734e7d6865964f298d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
